e65e11f1cdcdc3e0c5a533adba0e501c6d0e4758
ppesterev <ppesterev@mail.ru>
2021-06-15 17:13:47 +0700

    add function to validate strip requests

---
+++ src/js/strip-request-validation.js
@@ -0,0 +1,21 @@
+import { getCurrentStripData } from "./api";
+
+const validateRequest = (address) => {
+  return getCurrentStripData().then(({ num: maxNum }) => {
+    const requestedNumber = parseInt(address);
+    const isRequestedNumberValid =
+      !isNaN(requestedNumber) &&
+      requestedNumber >= 1 &&
+      requestedNumber <= maxNum;
+
+    if (isRequestedNumberValid) {
+      return address;
+    } else if (address === "random") {
+      return Math.floor(Math.random() * maxNum + 1).toString();
+    } else {
+      return "";
+    }
+  });
+};
+
+export { validateRequest };
