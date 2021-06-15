508b2b345385f0fe986e3701b7f47a9b99953817
ppesterev <ppesterev@mail.ru>
2021-06-15 22:37:53 +0700

    add module to update navigation links

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,26 @@
-import { navigateTo, onNavigate } from "./hash-navigation";
+import { redirectTo, onNavigate } from "./hash-navigation";
 import { validateRequest } from "./strip-request-validation";
 import { getStripData } from "./api";
 import { updateDisplayedStrip } from "./strip-display";
 
+import "./navbar";
+import { updateNavbar } from "./navbar";
+
 onNavigate((address) => {
   validateRequest(address).then((validAddress) => {
+    console.log(address, validAddress);
     if (address !== validAddress) {
-      return navigateTo(validAddress);
+      return redirectTo(validAddress);
     }
 
-    getStripData(validAddress).then(updateDisplayedStrip);
+    getStripData(validAddress).then((stripData) => {
+      updateDisplayedStrip(stripData);
+      getStripData().then((latestStripData) => {
+        displayedNum = stripData.num;
+        latestNum = latestStripData.num;
+
+        updateNavbar(displayedNum, latestNum);
+      });
+    });
   });
 });
---
+++ src/js/navbar.js
@@ -0,0 +1,23 @@
+import { navigateTo, onNavigate } from "./hash-navigation";
+
+const navbar = document.querySelector(".navigation");
+
+const links = navbar.querySelectorAll("a");
+links.forEach((link) => {
+  link.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    navigateTo(new URL(link.href).searchParams.get("n"));
+  });
+});
+
+const prevLink = navbar.querySelector("[data-navlink-prev]");
+const nextLink = navbar.querySelector("[data-navlink-next]");
+const latestLink = navbar.querySelector("[data-navlink-latest]");
+
+const updateNavbar = (displayedStripNum, latestStripNum) => {
+  prevLink.href = `?n=${displayedStripNum - 1}`;
+  nextLink.href = `?n=${displayedStripNum + 1}`;
+  latestLink.href = `?n=${latestStripNum}`;
+};
+
+export { updateNavbar };
