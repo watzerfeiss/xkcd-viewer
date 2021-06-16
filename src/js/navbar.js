d47de7e2778ed19870816e02135c5eaf06823705
ppesterev <ppesterev@mail.ru>
2021-06-16 11:49:50 +0700

    disable forward/back links when on 1st/last strip

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -10,12 +10,27 @@ links.forEach((link) => {
   });
 });
 
+const firstLink = navbar.querySelector("[data-navlink-first]");
 const prevLink = navbar.querySelector("[data-navlink-prev]");
 const nextLink = navbar.querySelector("[data-navlink-next]");
+const latestLink = navbar.querySelector("[data-navlink-latest]");
 
 const updateNavbar = (displayedStripNum, latestStripNum) => {
-  prevLink.href = `?n=${displayedStripNum - 1}`;
-  nextLink.href = `?n=${displayedStripNum + 1}`;
+  if (displayedStripNum === 1) {
+    prevLink.removeAttribute("href");
+    firstLink.removeAttribute("href");
+  } else {
+    prevLink.href = `?n=${displayedStripNum - 1}`;
+    firstLink.href = "?n=1";
+  }
+
+  if (displayedStripNum === latestStripNum) {
+    nextLink.removeAttribute("href");
+    latestLink.removeAttribute("href");
+  } else {
+    nextLink.href = `?n=${displayedStripNum + 1}`;
+    latestLink.href = "?";
+  }
 };
 
 export { updateNavbar };
