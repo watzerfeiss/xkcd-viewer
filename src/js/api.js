8de1ef067ca2d951924f8842712b094d59c4d107
ppesterev <ppesterev@mail.ru>
2021-06-16 16:09:41 +0700

    go back to original xkcd api with own cors proxy

--- src/js/api.js
+++ src/js/api.js
@@ -1,10 +1,11 @@
+const PROXY_URL = "https://eigencors.herokuapp.com/";
 const LATEST_STRIP_LIFETIME = 1000 * 60 * 5;
 
 let latestStrip = null;
 let latestStripTimestamp = Date.now();
 
 const request = (url) => {
-  return fetch(url)
+  return fetch(`${PROXY_URL}${url}`)
     .then((response) => response.json())
     .catch(() => {
       throw new Error("Failed to load comic data");
@@ -16,7 +17,7 @@ const getLatestStrip = () => {
     !latestStrip || Date.now() - latestStripTimestamp > LATEST_STRIP_LIFETIME;
 
   if (isLatestStripStale) {
-    return request("https://xkcd.now.sh/?comic=latest").then((strip) => {
+    return request("https://xkcd.com/info.0.json").then((strip) => {
       latestStrip = strip;
       latestStripTimestamp = Date.now();
       return strip;
@@ -26,6 +27,6 @@ const getLatestStrip = () => {
 };
 
 const fetchStrip = (num) =>
-  num ? request(`https://xkcd.now.sh/?comic=${num}`) : getLatestStrip();
+  num ? request(`https://xkcd.com/${num}/info.0.json`) : getLatestStrip();
 
 export { fetchStrip, getLatestStrip };
