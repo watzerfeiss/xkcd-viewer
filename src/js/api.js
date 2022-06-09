23dee41b70a79a03537651adad30e65f53ab2213
ppesterev <p.a.pesterev@gmail.com>
2022-06-09 20:34:44 +0700

    add function to fetch archive & parse strip list

--- src/js/api.js
+++ src/js/api.js
@@ -1,15 +1,18 @@
 const PROXY_URL = "https://eigencors.herokuapp.com/";
 const LATEST_STRIP_LIFETIME = 1000 * 60 * 5;
+const LIST_URL = "https://xkcd.com/archive/";
 
 let latestStrip = null;
 let latestStripTimestamp = Date.now();
 
-const request = (url) => {
+let stripList = null;
+
+const request = (url, asText = false) => {
   return fetch(`${PROXY_URL}${url}`).then((response) => {
     if (!response.ok) {
       throw new Error(response.status);
     }
-    return response.json();
+    return asText ? response.text() : response.json();
   });
 };
 
@@ -30,4 +33,32 @@ const getLatestStrip = () => {
 const fetchStrip = (num) =>
   num ? request(`https://xkcd.com/${num}/info.0.json`) : getLatestStrip();
 
-export { fetchStrip, getLatestStrip };
+const getStripList = () => {
+  if (stripList) {
+    return Promise.resolve(stripList.slice());
+  }
+
+  return request(LIST_URL, true).then((archiveHtml) => {
+    const archiveDoc = new DOMParser().parseFromString(
+      archiveHtml,
+      "text/html"
+    );
+
+    const fetchedList = Array.from(archiveDoc.links)
+      .filter((link) => {
+        return (
+          link.pathname.match(/^\/[0-9]+\/$/) &&
+          new Date(link.title).toString() !== "Invalid Date"
+        );
+      })
+      .map((link) => ({
+        number: link.pathname.slice(1, link.pathname.length - 1),
+        date: new Date(link.title),
+        title: link.textContent
+      }));
+    stripList = fetchedList;
+    return fetchedList;
+  });
+};
+
+export { fetchStrip, getLatestStrip, getStripList };
